2130e9f14fb61bce78b631328e043b9493580670
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-06-01 21:53:01 +0500

    work complete

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>naya app banan shuru krdya he</Text>
+      <Text>naya app banan shuru krdya he chlo bhai log</Text>
      
     </View>
   );
