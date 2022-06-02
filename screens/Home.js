c636cb95cd96ce7b4fc82109e10cd2b89860fc5d
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-06-02 21:35:42 +0500

    work complete

--- screens/Home.js
+++ screens/Home.js
@@ -2,10 +2,14 @@ import {Text, View, SafeArea, FlatList} from 'react-native'
 //flatlist native me as a.map he
 import {useState} from 'react'
 import {COLORS, NFTData} from '../constants'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Home = () => {
   return (
-    <Text>Home</Text>
+   <SafeAreaView style={{flex: 1}}>
+       
+
+   </SafeAreaView>
   )
 }
 
