ec46b30cb7de739bcb36fc9faa4c8e76e194fe9c
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-06-02 21:50:12 +0500

    work complete

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,17 @@
-import {Text, View, SafeArea, FlatList} from 'react-native'
+import {Text, View, SafeAreaView, FlatList} from 'react-native'
 //flatlist native me as a.map he
 import {useState} from 'react'
+
 import {COLORS, NFTData} from '../constants'
-import { SafeAreaView } from 'react-native-safe-area-context'
+// import { SafeAreaView } from 'react-native-safe-area-context'
+import {NFTCard, HomeHeader, FocusedStatusBar} from '../components'
 
 const Home = () => {
   return (
    <SafeAreaView style={{flex: 1}}>
-       
+
+       <FocusedStatusBar background={COLORS.primary}/>
+
 
    </SafeAreaView>
   )
