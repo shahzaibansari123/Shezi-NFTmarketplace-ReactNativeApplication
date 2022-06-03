9deeb0f7d46f934fb246335cb07f567b3ca649f5
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-06-03 22:43:22 +0500

    work complete

--- screens/Home.js
+++ screens/Home.js
@@ -20,6 +20,19 @@ const Home = () => {
             showsVerticalScrollIndicator={false}
           />
         </View>
+        <View
+          style={{
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            right: 0,
+            left: 0,
+            zIndex: -1,
+          }}
+        >
+          <View style={{height: 300, backgroundColor: COLORS.primary}}/>
+          <View style={{flex: 1 , backgroundColor: COLORS.white}}/>
+        </View>
       </View>
     </SafeAreaView>
   );
