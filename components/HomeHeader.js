85b34b1f565f6ac2e8fe833149589fc4af747bfe
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-06-05 08:22:36 +0500

    work complete

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -11,9 +11,35 @@ const HomeHeader = () => {
           alignItems: "center",
         }}
       >
-        <Image source={assets.logo} style={{width: 100 , height: 70, marginLeft: -30}} resizeMode="contain" />
+        <Image
+          source={assets.logo}
+          style={{ width: 70, height: 65 }}
+          resizeMode="contain"
+        />
+        <View style={{ width: 45, height: 45 }}>
+          <Image
+            source={assets.person01}
+            resizeMode="contain"
+            style={{ width: "100%", height: "100%" }}
+          />
+          <Image
+            source={assets.badge}
+            resizeMode="contain"
+            style={{ width: 15, height: 15 , position: "absolute" , bottom: 0, right: 0}}
+          />
+        </View>
       </View>
-      
+      <View style={{marginVertical: SIZES.font}}>
+        <Text style={{fontSize: SIZES.small, fontFamily: Fonts.regular, color: COLORS.white}}>
+          hello, Shahzaib 👋 
+
+        </Text>
+        <Text style={{fontSize: SIZES.small, fontFamily: Fonts.regular, color: COLORS.white}}>
+          hello, Shahzaib 👋 
+
+        </Text>
+
+      </View>
     </View>
   );
 };
