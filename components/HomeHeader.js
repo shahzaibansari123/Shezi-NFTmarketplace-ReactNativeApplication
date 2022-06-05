d63278be7888fb03715f8aeaf1c85d0227ead0cf
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-06-05 08:09:36 +0500

    work complete

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,12 +1,21 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TextInput } from "react-native";
+import { COLORS, SIZES, SHADOWS, assets } from "../constants";
 
 const HomeHeader = () => {
   return (
-    <View>
-      <Text>HomeHeader</Text>
+    <View style={{ backgroundColor: COLORS.primary, padding: SIZES.font }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Image source={assets.logo} style={{width: 100 , height: 70, marginLeft: -30}} resizeMode="contain" />
+      </View>
+      
     </View>
-  )
-}
+  );
+};
 
-export default HomeHeader
+export default HomeHeader;
