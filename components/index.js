53e47c34391d97bf401513f3333a1f46c305d947
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-06-06 22:18:38 +0500

    work complete

--- components/index.js
+++ components/index.js
@@ -2,5 +2,7 @@ import FocusedStatusBar from './FocusedStatusBar'
 import HomeHeader from './HomeHeader'
 import NFTCard from './NFTCard'
 import {CircleButton, RectButton} from './Button'
+import DetailsDesc from './DetailsDesc'
+import DetailsBid from './DetailsBid'
 
-export {NFTCard, HomeHeader, FocusedStatusBar, CircleButton, RectButton}
+export {NFTCard, HomeHeader, FocusedStatusBar, CircleButton, RectButton, DetailsDesc, DetailsBid}
