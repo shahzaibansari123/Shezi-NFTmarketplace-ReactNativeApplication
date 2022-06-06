ff540003ee1de3145c349488168d39677a3359f3
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-06-06 22:21:11 +0500

    work complete

--- screens/Details.js
+++ screens/Details.js
@@ -2,7 +2,8 @@ import {Text, View, SafeAreaView, Image, StatusBar, FlatList} from 'react-native
 import { COLORS, SHADOWS, SIZES, FONTS, assets } from "../constants";
 import {CircleButton, RectButton, FocusedStatusBar, SubInfo, DetailsDesc, DetailsBid} from '../components'
 
- const Details = () => {
+ const Details = ({route, navigation}) => {
+   console.log(route)
   return (
     <Text>Details</Text>
   )
