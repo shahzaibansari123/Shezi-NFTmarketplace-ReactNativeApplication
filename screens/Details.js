57f25bb4ab6507acda4f3798570eb2d337cd8aff
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-06-08 23:49:27 +0500

    work complete

--- screens/Details.js
+++ screens/Details.js
@@ -17,7 +17,7 @@ import {
 } from "../components";
 
 const Details = ({ route, navigation }) => {
-  const { data } = route.params
+  const { data } = route.params;
   // console.log(
   //   `------------------------------------------------`
   // );
@@ -25,16 +25,29 @@ const Details = ({ route, navigation }) => {
   // console.log(
   //   `------------------------------------------------`
   // );
-  return(
-   <SafeAreaView style={{flex: 1}}>
-     <FocusedStatusBar 
-     barStyle="dark-content"
-     backgroundColor="transparent"
-     translucent={true}
-     />
-
-   </SafeAreaView>
-  )
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent={true}
+      />
+      <View
+        style={{
+          width: "100%",
+          position: "absolute",
+          bottom: 0,
+          paddingVertical: SIZES.font,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "rgba(255, 255, 255, 0.5)",
+          zIndex: 1,
+        }}
+      >
+        <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark} />
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default Details;
