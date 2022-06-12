f98b1c96d4268ae6ce980d8d4519df11416af4eb
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-06-12 21:23:32 +0500

    work complete

--- components/DetailsDesc.js
+++ components/DetailsDesc.js
@@ -22,7 +22,7 @@ const DetailsDesc = ({ data }) => {
         />
         <EthPrice price={data.price} />
       </View>
-      <View style={{marginVertical: SIZES.extraLarge}}> 
+      <View style={{marginVertical: SIZES.extraLarge * 1.5}}> 
 
       </View>
     </>
